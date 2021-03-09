bb66ed54b636e45cbcd7073074276c32c78db5f7
bhupindersd <bhupinderbob@yahoo.com>
2021-03-09 19:56:48 +0000

    Add a game menu, allowing the user to choose a game mode or quit.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,28 @@
 #include "board/GameBoard.cpp"
 #include "player/HumanPlayer.cpp"
 
-int main() {
+void playerVsComputer() {
   HumanPlayer humanPlayer;
   humanPlayer.setBoatsOnBoard();
+}
+
+void showMenu() {
+  while (true) { // Ask the user to enter a game mode until they choose to quit.
+    int option = getNumber("Please select a game mode: \n1. Player v Computer \n0. Reset", 0, 1);
+    switch(option) {
+    case 1:
+      playerVsComputer();
+      continue;
+    case 0:
+      std::cout << "Thanks for playing!" << std::endl;
+      return;
+    default:
+      std::cout << "Invalid option, please try again." << std::endl;
+      continue;
+    }
+  }
+}
+
+int main() {
+  showMenu();
 }
