a8030985e0c116da0960c86d6f3d8ef79f4f5ec9
bhupindersd <bhupinderbob@yahoo.com>
2021-03-19 21:41:16 +0000

    Create a player vs player game mode.

--- main.cpp
+++ main.cpp
@@ -41,12 +41,49 @@ void playerVsComputer() {
   }
 }
 
+void playerVsPlayer() {
+  std::cout << "Human Player 1." << std::endl;
+  HumanPlayer humanPlayer1;
+  humanPlayer1.setBoatsOnBoard();
+
+  std::cout << "Human Player 2." << std::endl;
+  HumanPlayer humanPlayer2;
+  humanPlayer2.setBoatsOnBoard();
+
+  while (true) {
+    std::cout << "Human Player 1." << std::endl;
+    Coordinate human1TorpedoLocation = humanPlayer1.nextTurn();
+    HitStatus human1HitStatus = humanPlayer2.getHitStatus(human1TorpedoLocation);
+    humanPlayer1.updateHitBoard(human1TorpedoLocation, human1HitStatus);
+    if (human1HitStatus == WIN) {
+      ::waitForUser("Press enter to end the game.\n");
+      break;
+    } else {
+      ::waitForUser("Press enter to end your turn.\n");
+    }
+
+    std::cout << "Human Player 2." << std::endl;
+    Coordinate human2TorpedoLocation = humanPlayer2.nextTurn();
+    HitStatus human2HitStatus = humanPlayer1.getHitStatus(human2TorpedoLocation);
+    humanPlayer2.updateHitBoard(human2TorpedoLocation, human2HitStatus);
+    if (human2HitStatus == WIN) {
+      ::waitForUser("Press enter to end the game.\n");
+      break;
+    } else {
+      ::waitForUser("Press enter to end your turn.\n");
+    }
+  }
+}
+
 void showMenu() {
   while (true) { // Ask the user to enter a game mode until they choose to quit.
-    int option = getNumber("Please select a game mode: \n1. Player v Computer \n0. Quit", 0, 1);
+    int option = getNumber("Please select a game mode: \n1. Player v Computer\n2. Player v Player \n0. Quit", 0, 2);
     switch(option) {
     case 1:
       playerVsComputer();
+      continue;
+    case 2:
+      playerVsPlayer();
       continue;
     case 0:
       std::cout << "Thanks for playing!" << std::endl;
